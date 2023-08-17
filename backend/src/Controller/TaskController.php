e329b347aeef9add3166936bb7b55458fa69de13
Ofilio Herdocia Aráuz <Herdocia20@gmail.com>
2023-08-17 17:17:35 -0600

    CRUD TaskController

--- backend/src/Controller/TaskController.php
+++ backend/src/Controller/TaskController.php
@@ -22,19 +22,40 @@ class TaskController extends AbstractController
         $this->serializer = $serializer;
     }
 
-    #[Route('/task', name: 'app_task', methods: ['GET', 'HEAD'])]
+    #[Route('/task', name: 'list_tasks', methods: ['GET', 'HEAD'])]
     public function index(): JsonResponse
     {
         $tasks = $this->repository->findBy([
             'task_father' => null
         ]);
+        return $this->json($this->serialize($tasks));
+    }
 
-        $serializedTasks = $this->serializer->normalize($tasks, null, ['groups' => ['main', 'task_children']]);
+    #[Route('/task/{task}', name: 'show_task', methods: ['GET', 'HEAD'])]
+    public function show(Task $task): JsonResponse
+    {
+        return $this->json($this->serialize($task));
+    }
 
+    #[Route('/task/{task}', name: 'delete_task', methods: ['DELETE'])]
+    public function delete(Task $task): JsonResponse
+    {
+        $this->repository->remove($task, true);
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TaskController.php',
-            'tasks' => $serializedTasks
+            'status' => true
         ]);
+    }
+
+    #[Route('/task/{task}', name: 'update_task', methods: ['UPDATE'])]
+    public function update(Task $task): JsonResponse
+    {
+        $this->repository->save($task, true);
+        return $this->json([
+            'status' => true
+        ]);
+    }
+
+    private function serialize($entity){
+        return $this->serializer->normalize($entity, null, ['groups' => ['main', 'task_children']]);
     }
 }
